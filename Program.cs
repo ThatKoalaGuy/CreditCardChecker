f475aaa3de3ad66f45f80a24e4d0488d60221ce9
Domen Koder <domen.koder@gmail.com>
2022-08-27 15:56:16 +0200

    I've tried a bunch of things but none worked, so I' m basicaly comitting just the template.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("CREDIT CARD CHECKER");
+            Console.WriteLine("Input your credit card number");
+
+            //take input from the user, parse it into an int64 and remove the last digit and save it in a variable and remove any whitespaces
+            long creditCardNumber = long.Parse(Console.ReadLine().Replace(" ", ""));
+
+
+            
+
+
+
+
+
+
+
+
+
         }
     }
 }
